88b27aa2f319b3b6cd8f8544be0cedf2dd98e22c
atman-lens <34531378+atman-lens@users.noreply.github.com>
2018-02-16 22:09:20 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,51 @@
+// SMOOTH SCROLLING
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  var $nav = $(".navigation");
+  var navScrolled = "navigation__scrolled";
+  var $win = $(window);
+  var winH = $win.height();
+
+  
+// STICKY NAVIGATION
+
+  $win.on("scroll", function () {
+    if ($(this).scrollTop() > winH) {
+      $nav.addClass(navScrolled);
+    } else {
+      $nav.removeClass(navScrolled);
+    }
+  });
+});
+
+// $(window).scroll(function(){
+//   if (this.scrollTop() > winH ) {
+//     nav.addClass(navScrolled);
+//   } else {
+//     nav.removeClass(navScrolled);
+//   }
+// });
+
