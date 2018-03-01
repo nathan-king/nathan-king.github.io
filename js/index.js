06d11cdd6f0816a2dc27e65b55fc1acc011e6951
atman-lens <34531378+atman-lens@users.noreply.github.com>
2018-02-28 23:21:04 -0500

    repair toggle and add particle.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-
-
 // SMOOTH SCROLLING
 
 $(document).ready(function(){
+
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
@@ -57,13 +56,125 @@ $(document).ready(function(){
   }); 
 
   // TOGGLE NAV BAR
-
+  
   $('.navigation__toggle').click(function(e) {
-    // $(this).toggleClass('active');
-    $('.navigation').toggleClass('active-fix', 8000);
-    $('.navigation__list').toggleClass('active', 8000);
-    $('.navigation-nav').toggleClass('active-nav', 8000);
-    $('.navigation__item').toggleClass('active-link', 8000);
-    e.preventDefault();
+    //  $('.navigation__list').toggleClass('active__list');
+     $(".navigation__list").css('height', '100%').slideToggle(500);
+    //  $(".navigation__list").toggleClass('navigation__list--toggled')
+
+    // $('.navigation__list').toggleClass("navigation__list--active");
+    // $(this).siblings().toggleClass("navigation__list--active");
     });
+
+
+    // PARTICLES
+
+    particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 700
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
 });
+
+
+// toggleClass('active-nav', 8000);
